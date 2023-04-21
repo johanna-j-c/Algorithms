d8bae0f63532a301c40cf58ba2c5ba480b841db0
Johanna Cabanban <johanna.cabanban@gmail.com>
2023-04-21 10:55:39 -0400

    Create file for arrays3

---
+++ arrays3.py
@@ -0,0 +1,6 @@
+# Given an array, reverse the order of values without using the built-in method reverse or slice, 
+# ie. [: :-1]. Return the same array. Don't forget edge cases!
+
+# Example Input: [1, 2, 3, 4, 5, 6]
+# Example Output: [6, 5, 4, 3, 2, 1]
+
