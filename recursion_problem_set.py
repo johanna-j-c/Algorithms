74800a54665b9edcaffc50fe1b8a2a04fe9ca618
Johanna Cabanban <johanna.cabanban@gmail.com>
2023-05-06 16:57:44 -0400

    Complete nested parens problem

--- recursion_problem_set.py
+++ recursion_problem_set.py
@@ -1,9 +1,23 @@
-def reverse(text):
-    if not text:
-        return ""
+# Write a function reverse that accepts a string text as a parameter. 
+# It returns the reverse of text by reversing all characters in the string.
 
-    print(text)
+# def reverse(text):
+#     if not text:
+#         return ""
+
+#     print(text)
     
-    return text[-1] + reverse(text[0:-1])
+#     return text[-1] + reverse(text[0:-1])
 
-print(reverse("cat"))
+# print(reverse("cat"))
+
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    elif parens == "))" or parens == "((":
+        return False
+    
+    return is_nested_parens(parens[1:-1])
+
+
+print(is_nested_parens("(())))"))
